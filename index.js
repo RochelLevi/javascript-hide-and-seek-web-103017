2467aa3fe048c8b7e91a40bc1169b3dece03cd7f
RochelLevi <rochlevi@gmail.com>
2017-12-14 10:48:12 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector(`#nested .target `)
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n
+  }
+}
+
+function deepestChild(){
+  let node = document.querySelector('div#grand-node')
+  while (node.firstElementChild){
+    node = node.firstElementChild
+  }
+  return node
+}
